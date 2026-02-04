7bbdbe16c2c4f836e921c1e2bd74befb18b9f56f
Your Name <you@example.com>
2026-02-04 23:36:56 +0530

    ep-2

--- ep1 practice/App.js
+++ ep1 practice/App.js
@@ -1,25 +1,14 @@
 /*
-<div id="parent">
-  <div id="child">
-   <h1>THis is h1 tag</h1>
-   <h2>This is h2 tag</h2>
-  </div>
-</div>
-
+->) intialize the git, adding remote file to origin main from remote react-practice repozitory
+->) intialize the npm init so further package.json set up and further dependencies also.
+->) bundler-> this is bundle app for app.js,css,html .it bundle so that ship to production.
+->) webpack,parcel,vite - these are bundler.
+->) as use parel bundler 
+->) install npm install -D parcel
+->) two type of dependencies - dev and normal dependencies.so it -D  install as devdependencies.
+->"^2.8.3"  version as new minor version for "^2.8.4" ,, "~2.8.4" major version
+->package.json => it keep dependencies or configuration,approx dependencies , but in package-lock.json there is exact version as keep track of versions.and same tell for in development and in production there versions differences not come.
+->node_modules => it actall data 
+-> transcitive dependencies-as dependencies dependent on some other dependencies.
+-> in .gitignore there put file which not push to github.there pack.json and package-lock.json put in github as there in all used dependencies.
 */
-
-const heading = React.createElement(
-    'div',{id:"parent"},[
-        React.createElement('div',{id:"child1"},
-        [React.createElement("h1",{},"This is h1 tag"),
-         React.createElement("h2",{},"This is h1 tag")]),
-        React.createElement('div',{id:"child2"},
-        [React.createElement("h1",{},"This is h1 tag"),
-         React.createElement("h2",{},"This is h1 tag")])
-]);
-
-
-console.log(heading);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);
