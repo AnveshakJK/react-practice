b96ff9b89c20b8dd6e3bc5f5c40b6403445cd254
Your Name <you@example.com>
2026-02-07 10:35:17 +0530

    there is some info about react,transpiler, reactelement,jsx

--- ep1 practice/App.js
+++ ep1 practice/App.js
@@ -1,53 +1,18 @@
-/*
-->) intialize the git, adding remote file to origin main from remote react-practice repozitory
-->) intialize the npm init so further package.json set up and further dependencies also.
-->) bundler-> this is bundle app for app.js,css,html .it bundle so that ship to production.
-->) webpack,parcel,vite - these are bundler.
-->) as use parel bundler 
-->) install npm install -D parcel
-->) two type of dependencies - dev and normal dependencies.so it -D  install as devdependencies.
-->"^2.8.3"  version as new minor version for "^2.8.4" ,, "~2.8.4" major version
-->package.json => it keep dependencies or configuration,approx dependencies , but in package-lock.json there is exact version as keep track of versions.and same tell for in development and in production there versions differences not come.
-->node_modules => it actall data 
--> transcitive dependencies-as dependencies dependent on some other dependencies.
--> in .gitignore there put file which not push to github.there pack.json and package-lock.json put in github as there in all used dependencies.
+import React from "react";
+import ReactDOM from "react-dom/client";
 
--- install 
--> npx parcel index.html ,,,, in npx is for executing the package 
--> as download node_module from data and it package are fetched otherwise from server it get costier.
--> there in package.json there versions are also mentained 
-->install react as in normal dependencies
-npm install react
-->npm install react-dom
+const react = React.createElement("div",{id:"heading "},"this is the object");
+const jsxHeading = <h1 id="heading">This is the jsx syntax</h1>
 
-->)) parcel is help in 
-=> dev build
-=> local server
-=> HMR = hot module replacement.(whenever the changing then in module then change in help module.)
-=> file watching alogrithm - written in c++
-=>caching - faster builds
-=>Image optimization.
-=> minification
-=> building
-=> compress
-=> consistent hashing
-=> code splitting
-=> differential bundling - (open in variation of browser so it support older browser)
-=> Diagnostic
-=> error handling 
-=> host in HTTPs/SSL
-=> Tree shaking - remove unused code.
-=> different dev and production bundles
+console.log(jsxHeading);
+const reactdom = ReactDOM.createRoot(document.getElementById("root"));
 
-// read documentation of parcel. 
+//JSX -it a syntax and createElement is a react element
+//creating a react element using jsx
+// Babel(inside parcel) is the helper (transpiler (one code to another code like ES6 conversion also)) for jsx into react element conversion.
+// like in compiler there parsing is done and all like that things as happen in babel;
 
-// for production level  build of parcel as do--> npx parcel build index.html
+// attributes and tags in inside jsx 
+// if you write in multiple line then wrap in () . as due to understand it one and compiled to.
 
-// for parcel module in dist,index.html,index these code for production level.
-
-->)) for setting the which browser should i supported are listed in browserList in node_module.
-so in package.json of this package browserlist there add dependencies as 
-"browserList":{"last 2 chrome version","last 2 firefox version"}
-so it mean might be work be on other browser but in these definitely work.
-
-*/
+reactdom.render(jsxHeading);
