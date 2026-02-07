3a501fc3429f8468ff008b7d7b6d1422f9490bdb
Your Name <you@example.com>
2026-02-07 11:22:35 +0530

    react element,react component,jsx sanitization, browser infinite loop , element in component and vice versa, coomponent in functinal call way , js use in functional component

--- ep1 practice/App.js
+++ ep1 practice/App.js
@@ -5,14 +5,54 @@ const react = React.createElement("div",{id:"heading "},"this is the object");
 const jsxHeading = <h1 id="heading">This is the jsx syntax</h1>
 
 console.log(jsxHeading);
+
+//React component - 2types -: class and function component
+/* 
+-normal javascript function -react component
+-function component - a functional component return jsx
+*/
+const HeadingComponent = ()=>{
+    return <h1>Namaste react functional component</h1>
+}  
+ // || both below and above are same
+//  //component composition - component inside component
+
+const number = 10000*5;
+// react element use as this way
+const ele = <span>React element</span>
+
+const Title = ()=>{
+    return (<div>  
+        <h1 className="head" tabIndex="5" >Namaste react</h1>
+        <HeadingComponent/>
+        </div>
+    );
+} // in this fuctional way there return is used then it work other use of () write in jsx return. suppose of react element is define first and react component is make after react element but in react element there call of react component is first then get error of can't access before intialize  .
+
+const HeadinComp = ()=>(
+    <div>
+        <Title/>
+        {ele*5000}
+        {/* inject js */}
+        {number} 
+        {/* using same way function component like function call */}
+        {Title()}
+        <h1>Namaste react functional component</h1>
+    </div>
+)
+
+// in jsx run js by {} use.
+
 const reactdom = ReactDOM.createRoot(document.getElementById("root"));
 
-//JSX -it a syntax and createElement is a react element
-//creating a react element using jsx
-// Babel(inside parcel) is the helper (transpiler (one code to another code like ES6 conversion also)) for jsx into react element conversion.
-// like in compiler there parsing is done and all like that things as happen in babel;
+// in js cant use of const varibale before intialization ,,like of function decleration after before it use.
 
-// attributes and tags in inside jsx 
-// if you write in multiple line then wrap in () . as due to understand it one and compiled to.
+// as we do component in element and element in component also 
 
-reactdom.render(jsxHeading);
+
+// how the data escape and do data sanitization.***(jsx is already done data sanintization)** so that cross site scritping is not done like 
+
+// const data = 100 use in component as {data} then through browser as js can manipulate or change thing but jsx is take care of it 
+
+// reactdom.render(jsxHeading);
+reactdom.render(<HeadinComp/>);
